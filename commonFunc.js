1722dd6aff440cf5e50b990824e27972d44fc3d3
lunglungyu <lunglungyu2004@gmail.com>
2016-02-24 21:55:40 +0800

    workerProducer prototype

--- commonFunc.js
+++ commonFunc.js
@@ -1,11 +1,15 @@
 'use strict';
 const libRequest = require('request');
 const libHttpStatusCodes = require('http-status-codes');
+const DECIMAL_NEED  = 2;
+function isValidFunction(varFunc){
+  return (varFunc!== null && varFunc instanceof Function);
+}
 class CommonFunc {
   CommonFunc() {
 
   }
-  getBeanstalkdServerInfo(restapi_url, restapi_key){
+  getBeanstalkdServerInfo(restapi_url, restapi_key, callback){
     var postReq = {
       url: restapi_url,
       headers: {
@@ -15,10 +19,20 @@ class CommonFunc {
     };
     libRequest.post(postReq,function (error, response, body) {
       if (!error && response.statusCode == libHttpStatusCodes.OK) {
-        console.log(body) // Show the HTML for the Google homepage.
+        //console.log(body) // Show the HTML for the Google homepage.
+        if(isValidFunction(callback)){
+          callback(body);
+        }
+        return body;
       }else{
+        console.log(error);
       }
-    })
+    });
+    return null;
+  }
+  getParsedRateString(rate){
+    return parseFloat(rate).toFixed(DECIMAL_NEED);
   }
 }
-module.exports = CommonFunc;
+module.exports = new CommonFunc();
+//module.exports = CommonFunc;   
--- workerProducer.js
+++ workerProducer.js
@@ -3,18 +3,16 @@ const RESTAPI_KEY = 'a6403a2b-af21-47c5-aab5-a2420d20bbec';
 const TUBE_NAME = 'lunglungyu'
 const DOLLAR_SRC = 'HKD';
 const DOLLAR_DST = 'USD';
-
 const libNodestalker = require('nodestalker');
 const libCheer = require('cheerio');
-const libMongo = require('mongodb');
 const libRequest = require('request');
 const libHttpStatusCodes = require('http-status-codes');
-const moduleCommonFunc = require('./commonFunc')
-function createTask(beanStalk_url) {
-  var client = libNodestalker.Client(beanStalk_url);
+const moduleCommonFunc = require('./commonFunc');//cache the same instance
+var number_of_job_to_create = 1;
+function createTask(beanstalkd_url) {
+  var client = libNodestalker.Client(beanstalkd_url);
   client.use(TUBE_NAME).onSuccess(function(data) {
-    console.log('use onSuccess:'+data);
-    console.log(data);
+    console.log('use onSuccess:'+data); // array containing TUBE_NAME
     client.put(JSON.stringify({
       'from':  DOLLAR_SRC,
       'to': DOLLAR_DST,
@@ -25,7 +23,30 @@ function createTask(beanStalk_url) {
     });
   });
 }
-//createTask();
-//getBeanstalkdServerInfo();
-var commonFunc = new moduleCommonFunc();
-var serverINfo = commonFunc.getBeanstalkdServerInfo(RESTAPI_FOR_BEANSTALKD_SERVER,RESTAPI_KEY);
+/**
+function default parameters may not be enabled in old version v8 engine , wrap a simple One
+*/
+function createTaskRepeated(beanstalkd_url, count){
+  for(var i = 0; i < count; ++i){
+    createTask(beanstalkd_url);
+  }
+}
+function runJob(server_info){
+  var beanstalkd_url = server_info.data.host + ":" + server_info.data.port;
+  //console.log(beanstalkd_url);
+  createTaskRepeated(beanstalkd_url,number_of_job_to_create);
+}
+function workerProducer(){
+  var args_number = process.argv.length;
+  //console.log('Number of argument :'+args_number);
+  if(args_number>2){
+    number_of_job_to_create=parseInt(process.argv.slice(2));
+  }else{
+    console.log("Usage: node workerProducer.js (optional,default 1)number_of_job_to_create");
+  }
+  console.log('Number of job to create:'+number_of_job_to_create);
+  //commonFunc.getBeanstalkdServerInfo(RESTAPI_FOR_BEANSTALKD_SERVER,RESTAPI_KEY,null);
+  var server_info = moduleCommonFunc.getBeanstalkdServerInfo(RESTAPI_FOR_BEANSTALKD_SERVER,RESTAPI_KEY,runJob);
+
+}
+workerProducer();
